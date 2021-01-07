905b35eaefd3798a070750a41538f400ab6d1ddc
pressname <pressname@protonmail.ch>
2021-01-07 16:29:52 +0100

    Added params

--- HelloWorld/helloworld.c
+++ HelloWorld/helloworld.c
@@ -1,10 +1,24 @@
 #include <linux/module.h>
 #include <linux/kernel.h> //Debug messages
 #include <linux/init.h>
+#include <linux/moduleparam.h>
+#include <linux/stat.h>
+
+#define DRIVER_AUTHOR "pressname"
+#define DRIVER_DESC "some text"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR(DRIVER_AUTHOR);
+MODULE_DESCRIPTION(DRIVER_DESC);
+MODULE_SUPPORTED_DEVICE("test_device");
+
+static char *MyString = "";
+module_param(MyString, charp, 0000);
+MODULE_PARM_DESC(MyString, "Hello World!");
 
 static int HelloInit(void){
 
-  printk(KERN_INFO "ROOTKITDEV_DEBUG: HELLO WORLD! \n");
+  printk(KERN_INFO "ROOTKITDEV_DEBUG: %s \n", MyString);
   return 0;
 }
 
@@ -16,5 +30,3 @@ static void HelloExit(void){
 
 module_init(HelloInit);
 module_exit(HelloExit);
-
-MODULE_LICENSE("GPL");
